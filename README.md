83646c85fd8707579579ff253580ed4d34b624fe
Kay Shoji <kay.shoji@auckland.ac.nz>
2019-10-30 11:55:37 +1300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# vue-vuetify-sample
+# sampel-app
+
+This is the sample app with Vue.js and Vuetify.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+
+    <v-card class="pa-4">
+      <v-card-title>Sample Forms</v-card-title>
+
+      <v-switch
+              :input-value=items[0].active
+              :label="`${items[0].id}`"
+      ></v-switch>
+
+      <v-switch
+              color="amber"
+              :input-value=items[1].active
+              :label="`${items[1].id}`"
+      ></v-switch>
+
+      <v-checkbox
+              v-model="checkbox"
+              :label="`Checkbox 1: ${checkbox.toString()}`"
+      ></v-checkbox>
+      <v-radio-group v-model="radioGroup">
+        <v-radio
+                v-for="n in 3"
+                :key="n"
+                :label="`Radio ${n}`"
+                :value="n"
+        ></v-radio>
+      </v-radio-group>
+
+
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+    data () {
+      return {
+        items: [
+          { id: "id 001", active: true },
+          { id: "id 002", active: false },
+        ],
+        checkbox: true,
+        radioGroup: 1,
+      }
+    },
+
+};
+
+</script>
