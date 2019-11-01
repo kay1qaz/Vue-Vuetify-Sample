005fa6ef3f6a14784c668ddd7029c9ac81d6ac3c
Kay Shoji <kay.shoji@auckland.ac.nz>
2019-11-01 16:26:36 +1300

    add config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  pages: {
+    index: {
+      entry: 'src/main.js',
+    },
+  },
+  devServer: {
+    port: 5050,
+    host: 'localhost',
+  },
 }
